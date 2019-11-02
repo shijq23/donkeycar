b1fcf8f40eeec3493d567087a596abce195fe390
Jianqiang Shi <shijq@hotmail.com>
2019-11-02 16:52:44 -0400

    enable linting

--- donkeycar/parts/usbcam.py
+++ donkeycar/parts/usbcam.py
@@ -1,38 +1,39 @@
+import glob
 import os
-import cv2
 import time
+
+import cv2
 import numpy as np
 from PIL import Image
-import glob
 
 
-class PiCamera():
+class PiCamera:
     def __init__(self, resolution=(120, 160), framerate=7):
         self.video = cv2.VideoCapture(0)
-        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0]) #SCREEN_WIDTH
-        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1]) #SCREEN_HIGHT
-        print('video resolution h, w {0}'.format(resolution))
+        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])  # SCREEN_WIDTH
+        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])  # SCREEN_HIGHT
+        print("video resolution h, w {0}".format(resolution))
         # Find OpenCV version
-        print('OpenCV version %s' % cv2.__version__)
-        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-        
+        print("OpenCV version %s" % cv2.__version__)
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split(".")
+
         # With webcam get(CV_CAP_PROP_FPS) does not work.
         # Let's see for ourselves.
-        
+
         if int(major_ver) < 3:
             fps = self.video.get(cv2.cv.CV_CAP_PROP_FPS)
-            print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+            print("Frames per second: {0}".format(fps))
         else:
             fps = self.video.get(cv2.CAP_PROP_FPS)
-            print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+            print("Frames per second: {0}".format(fps))
 
         # initialize the frame and the variable used to indicate
         # if the thread should be stopped
         self.frame = None
         self.on = True
 
-        print('UsbCamera loaded.. .warming camera')
-        #time.sleep(2)
+        print("UsbCamera loaded.. .warming camera")
+        # time.sleep(2)
 
     def run(self):
         _, bgr_image = self.video.read()
@@ -53,6 +54,6 @@ class PiCamera():
     def shutdown(self):
         # indicate that the thread should be stopped
         self.on = False
-        print('stopping UsbCamera')
-        time.sleep(.5)
+        print("stopping UsbCamera")
+        time.sleep(0.5)
         self.video.release()
